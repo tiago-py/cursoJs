0d00ed6dcd819d410c25be78eb6de78ae1f518b9
tiago-py <tiago.braga0012@gmail.com>
2023-05-15 22:14:49 -0300

    first commit

---
+++ aula-2/script.js
@@ -0,0 +1,5 @@
+var nome1 = "Tiago";//variavel local
+let nome2 = "Gustavo";//variavel local
+const nome3 = "Pedro";//constante 
+nome4 = "João";//variavel global
+console.log(nome1, nome2, nome3, nome4);
