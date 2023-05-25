703f832ddaa58889fa0a3644f620d8fa92673d06
tiago-py <tiago.braga0012@gmail.com>
2023-05-25 10:48:51 -0300

    anotações livro

---
+++ livro/arrays.js
@@ -0,0 +1,29 @@
+//link do livro https://github.com/braziljs/eloquente-javascript/tree/master
+
+
+/*arrays são um conjunto de dados que ficam salvos na memória de uma váriavel
+e que podem ser acessados a qualquer momento.
+podem ser uteis para guardar uma grande quantidade de informações.
+como por exemplo uma lista de números, como o exemplo abaixo
+o array é escrito como uma lista de valores separados por vírgulas e entre colchetes.
+*/
+
+
+var listOfNumbers = [2, 3, 5, 7, 11];
+console.log(listOfNumbers[1]);
+// → 3
+console.log(listOfNumbers[1 - 1]);
+// → 2 
+
+//Alguns métodos de arrays
+var mack = [];
+mack.push("Mack");
+mack.push("the", "Knife");
+console.log(mack);
+// → ["Mack", "the", "Knife"]
+console.log(mack.join(" "));
+// → Mack the Knife
+console.log(mack.pop());
+// → Knife
+console.log(mack);
+// → ["Mack", "the"]
---
+++ livro/objetos.js
@@ -0,0 +1,95 @@
+/*Valores do tipo objeto são coleções arbitrárias de propriedades, sendo que 
+podemos adicionar ou remover essas propriedades da forma que desejarmos. 
+Uma maneira de criar um objeto é usando a notação com chaves.
+*/
+
+var day1 = {
+squirrel: false,
+events: ["work", "touched tree", "pizza", "running",
+            "television"]
+};
+console.log(day1.squirrel);
+// → false
+console.log(day1.wolf);
+// → undefined
+day1.wolf = false;
+console.log(day1.wolf);
+// → false
+
+/*
+Dentro das chaves, podemos informar uma lista de propriedades separadas por vírgulas. 
+Cada propriedade é escrita com um nome seguido de dois pontos e uma expressão que fornece 
+o valor da propriedade. 
+Espaços e quebras de linha não fazem diferença. 
+Quando um objeto se estende por várias linhas, indentá-lo, 
+como mostrado no exemplo anterior, melhora a legibilidade. 
+Propriedades cujos nomes não são variáveis ou números válidos precisam estar entre aspas.
+*/
+
+var descriptions = {
+work: "Went to work",
+"touched tree": "Touched a tree"
+};
+
+
+var journal = [
+    {events: [
+        "work",
+        "touched three",
+        "pizza",
+        "running",
+        "television"
+    ], 
+    squirrel:false},
+    {events: [
+        "work", 
+        "ice cream", 
+        "cauliflower",
+        "lasagna", 
+        "touched tree", 
+        "brushed teeth"
+    ],
+    squirrel: false},
+    {events: [
+        "weekend", 
+        "cycling", 
+        "break",
+        "peanuts", 
+        "beer"
+    ],
+    squirrel: true},
+];
+console.log(journal[0]);
+/*
+false    
+{
+events: [ 'work', 'touched three', 'pizza', 'running', 'television' ],
+squirrel: false
+}
+*/
+
+var object1 = {value: 10};
+var object2 = object1;
+var object3 = {value: 10};
+
+console.log(object1 == object2);
+// → true
+console.log(object1 == object3);
+// → false
+
+object1.value = 15;
+console.log(object2.value);
+// → 15
+console.log(object3.value);
+// → 10
+/*As variáveis object1 e object2 estão associadas ao mesmo objeto e, por isso, alterar 
+object1 também altera o valor de object2.
+*/
+
+/*
+Quando comparamos objetos, o operador == do JavaScript irá retornar true apenas se ambos 
+os objetos possuem exatamente o mesmo valor. 
+Comparar objetos diferentes irá retornar false mesmo se eles tiverem conteúdos 
+idênticos.
+*/
+
