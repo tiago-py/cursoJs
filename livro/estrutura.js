bdc43aab5e3ab07c4d13165567a74da4471e5cc3
tiago-py <tiago.braga0012@gmail.com>
2023-06-01 13:56:01 -0300

    estrutura de dados

--- livro/estrutura.js
+++ livro/estrutura.js
@@ -122,3 +122,16 @@ function tableFor(event, journal) {
 // → [76, 9, 4, 1]
 
 console.log(phi(tableFor("pizza", JOURNAL)));
+
+
+var map = {};
+function storePhi(event, phi){
+    map[event] = phi;
+}
+
+storePhi("pizza", 0.069);
+storePhi("touched tree", -0.081);
+console.log("pizza" in map);
+// → true
+console.log(map["touched tree"]);
+// → -0.081
