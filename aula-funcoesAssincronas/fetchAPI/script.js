865ae98226b22981823a041f1730031c1ac2af97
tiago-py <tiago.braga0012@gmail.com>
2023-10-05 16:37:49 -0300

    Funções assincronas

--- aula-funcoesAssincronas/fetchAPI/script.js
+++ aula-funcoesAssincronas/fetchAPI/script.js
@@ -1,44 +1,44 @@
-function obterDadosUsuario(nomeusuario){
-    const url = `https://github.com/users/${nomeusuario}`
+document.addEventListener('DOMContentLoaded', () => {
+    function obterDadosUsuario(nomeusuario) {
+        const url = `https://api.github.com/users/${nomeusuario}`;
 
-    return fetch(url)
-        .then((response)=>{
-            if(response.ok){
-                return response.json()
-            }else{
-                throw new Error('Erro ao obter dados do usuario')
-            }
-        })
-        .then((data)=>{
-            return data
-        })
-        .catch((error)=>{
-            console.log(`Erro: ${error}`)
-        })
+        return fetch(url)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Erro ao obter dados do usuário');
+                }
+            })
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                console.log(`Erro: ${error}`);
+            });
+    }
 
-}
+    obterDadosUsuario('tiago-py')
+        .then((usuario) => {
+            exibirDadosUsuario(usuario);
+        });
 
-obterDadosUsuario('tiago-py')
-    .then((usuario)=>{
-        exibirDadosUsuario(usuario)
-    })
+    function exibirDadosUsuario(usuario) {
+        const tableBody = document.querySelector('#dados-table tbody');
 
-function exibirDadosUsuario(usuario){
-    const tableBody = document.querySelector('#dados-table table')
+        const row = document.createElement('tr');
+        const nomeCell = document.createElement('td');
+        const reposCell = document.createElement('td');
+        const seguidorCell = document.createElement('td');
 
-    const row = document.createElement('tr')
-    const nomeCell = document.createElement('td')
-    const reposCell = document.createElement('td')
-    const seguidorCell = document.createElement('td')
+        nomeCell.textContent = usuario.name;
+        reposCell.textContent = usuario.public_repos;
+        seguidorCell.textContent = usuario.followers;
 
-    nomeCell.textContent = usuario.name
-    reposCell.textContent = usuario.public_repos
-    seguidorCell.textContent = usuario.followers
+        row.appendChild(nomeCell);
+        row.appendChild(reposCell);
+        row.appendChild(seguidorCell);
 
-    row.append(nomeCell)
-    row.append(reposCell)
-    row.append(seguidorCell)
-
-    tableBody.appendChild(row)
-}
-
+        tableBody.appendChild(row);
+    }
+});
